0c2631b9137900130c76ed48a04e104f1e05916a
Piotr Nowak <p.nowak2@gmail.com>
2019-12-14 14:09:22 +0100

    add and get todos working with html page

--- src/apps/console/main.app.ts
+++ src/apps/console/main.app.ts
@@ -2,27 +2,43 @@ import { TodoPresenter } from "../../features/todo/presentation/todo.presenter";
 import { GetAllTodosUseCase } from "../../features/todo/domain/usecase/get-all-todos.usecase";
 import { TodoRepository } from "../../features/todo/domain/repository/todo.repository";
 import { TodoInMemoryRepository } from "../../features/todo/data/repository/inmemory/todo.inmemory.repository";
-import { UserPresenter } from "../../features/user/presentation/user.presenter";
 import { Observable } from "rxjs";
 import { Todo } from "../../features/todo/presentation/state/todos.state";
+import { TodoLocalStorageRepository } from "../../features/todo/data/repository/localstorage/todo.localstorage.repository";
+import { LocalStorageService } from "../../core/domain/service/localstorage.service";
+import { LocalStorageBrowserService } from "../../core/data/service/localstorage-browser.service";
+import { AddTodoUseCase } from "../../features/todo/domain/usecase/add-todo.usecase";
 
 export class ConsoleApp {
   todos$: Observable<Array<Todo>>;
   todoPresenter: TodoPresenter;
-  userPresenter: UserPresenter;
 
   constructor() {
+    const localStorageService: LocalStorageService = new LocalStorageBrowserService(
+      window.localStorage
+    );
+    const localStorageTodoRepo: TodoRepository = new TodoLocalStorageRepository(localStorageService);
     const inMemoryTodoRepo: TodoRepository = new TodoInMemoryRepository();
     const getAllTodosUC: GetAllTodosUseCase = new GetAllTodosUseCase(inMemoryTodoRepo);
-
+    const addTodoUC: AddTodoUseCase = new AddTodoUseCase(inMemoryTodoRepo);
     this.todoPresenter = new TodoPresenter(
       getAllTodosUC,
+      addTodoUC
     );
+    this.todos$ = this.todoPresenter.todos$;
   }
 
   run() {
-    this.todoPresenter.getAllTodos().subscribe(todos => {
-      console.log('todos', todos);
+    this.todos$.subscribe(todos => {
+      console.log('got todos', todos);
+    });
+
+    document.querySelector('#getAllTodos').addEventListener('click', () => {
+      this.todoPresenter.getAllTodos();
+    });
+
+    document.querySelector('#addTodo').addEventListener('click', () => {
+      this.todoPresenter.addTodo('Hello!');
     });
   }
 }
--- src/features/todo/presentation/todo.presenter.ts
+++ src/features/todo/presentation/todo.presenter.ts
@@ -1,10 +1,10 @@
 import { GetAllTodosUseCase } from "../domain/usecase/get-all-todos.usecase";
-import { Subject, BehaviorSubject, Observable } from "rxjs";
-import { takeUntil, map, catchError, startWith } from "rxjs/operators";
+import { BehaviorSubject, Observable } from "rxjs";
+import { map, startWith } from "rxjs/operators";
 import { TodoViewModelMapper } from "./todo.mapper";
-import { TodoViewModel } from "./todo.viewmodel";
 import { Presenter } from "../../../core/presentation/presenter";
 import { TodoState, Todo } from "./state/todos.state";
+import { AddTodoUseCase } from "../domain/usecase/add-todo.usecase";
 
 export class TodoPresenter extends Presenter {
   private state = new TodoState();
@@ -14,22 +14,43 @@ export class TodoPresenter extends Presenter {
   todos$: Observable<Array<Todo>> = this.dispatch
     .asObservable()
     .pipe(
-      map(state => state.todos),
-      startWith([])
+      map(state => state.todos)
     );
-
 
   constructor(
     private getAllTodosUC: GetAllTodosUseCase,
+    private addTodoUC: AddTodoUseCase,
   ) {
     super();
   }
 
-  getAllTodos(): Observable<Array<Todo>> {
-    return this.getAllTodosUC
+  getAllTodos() {
+    this.getAllTodosUC
       .execute()
       .pipe(
         map(todos => todos.map(this.mapper.mapFrom)),
-      )
+      ).subscribe(todos => {
+        this.dispatch.next(
+          this.state = {
+            ...this.state,
+            todos
+          }
+        );
+      });
+  }
+
+  addTodo(name: string) {
+    this.addTodoUC
+      .execute(name)
+      .pipe(
+        map(todo => this.mapper.mapFrom(todo))
+      ).subscribe(todo => {
+        this.dispatch.next(
+          this.state = {
+            ...this.state,
+            todos: [...this.state.todos, todo]
+          }
+        );
+      })
   }
 }
