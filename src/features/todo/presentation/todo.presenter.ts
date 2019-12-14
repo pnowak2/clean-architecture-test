f4183003f98b6f6cced4395df052540854f46a8c
Piotr Nowak <p.nowak2@gmail.com>
2019-12-14 13:37:14 +0100

    funnelling todo$ through dispatch subject

--- src/features/todo/presentation/todo.presenter.ts
+++ src/features/todo/presentation/todo.presenter.ts
@@ -1,6 +1,6 @@
 import { GetAllTodosUseCase } from "../domain/usecase/get-all-todos.usecase";
 import { Subject, BehaviorSubject, Observable } from "rxjs";
-import { takeUntil, map, catchError } from "rxjs/operators";
+import { takeUntil, map, catchError, startWith } from "rxjs/operators";
 import { TodoViewModelMapper } from "./todo.mapper";
 import { TodoViewModel } from "./todo.viewmodel";
 import { Presenter } from "../../../core/presentation/presenter";
@@ -9,9 +9,15 @@ import { TodoState, Todo } from "./state/todos.state";
 export class TodoPresenter extends Presenter {
   private state = new TodoState();
   private dispatch = new BehaviorSubject<TodoState>(this.state);
-  
-  todos$: Subject<Array<TodoViewModel>> = new Subject<Array<TodoViewModel>>();
-  mapper = new TodoViewModelMapper();
+  private mapper = new TodoViewModelMapper();
+
+  todos$: Observable<Array<Todo>> = this.dispatch
+    .asObservable()
+    .pipe(
+      map(state => state.todos),
+      startWith([])
+    );
+
 
   constructor(
     private getAllTodosUC: GetAllTodosUseCase,
