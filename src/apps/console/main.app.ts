7159cef808deafd947d45477c7e5c8136458a8d2
Piotr Nowak <p.nowak2@gmail.com>
2019-12-14 11:45:39 +0100

    idea from medium article

--- src/apps/console/main.app.ts
+++ src/apps/console/main.app.ts
@@ -2,91 +2,25 @@ import { TodoPresenter } from "../../features/todo/presentation/todo.presenter";
 import { GetAllTodosUseCase } from "../../features/todo/domain/usecase/get-all-todos.usecase";
 import { TodoRepository } from "../../features/todo/domain/repository/todo.repository";
 import { TodoInMemoryRepository } from "../../features/todo/data/repository/inmemory/todo.inmemory.repository";
-import { SearchTodosUseCase } from "../../features/todo/domain/usecase/search-todos.usecase";
-import { AddTodoUseCase } from "../../features/todo/domain/usecase/add-todo.usecase";
-import { GetTodoByIdUseCase } from "../../features/todo/domain/usecase/get-todo-by-id.usecase";
-import { UserRepository } from "../../features/user/domain/repository/user.repository";
-import { UserInMemoryRepository } from "../../features/user/data/repository/inmemory/user.inmemory.repository";
-import { GetAllUsersUseCase } from "../../features/user/domain/usecase/get-all-users.usecase";
 import { UserPresenter } from "../../features/user/presentation/user.presenter";
-import { RemoveTodoUseCase } from "../../features/todo/domain/usecase/remove-todo-id.usecase";
-import { MarkTodoAsCompletedUseCase } from "../../features/todo/domain/usecase/mark-todo-as-complete.usecase";
-import { MarkTodoAsIncompletedUseCase } from "../../features/todo/domain/usecase/mark-todo-as-incomplete.usecase";
-import { GetCompletedTodosUseCase } from "../../features/todo/domain/usecase/get-completed-todos.usecase";
-import { GetIncompletedTodosUseCase } from "../../features/todo/domain/usecase/get-incompleted-todos.usecase";
-import { TodoLocalStorageRepository } from "../../features/todo/data/repository/localstorage/todo.localstorage.repository";
-import { LocalStorageBrowserService } from "../../core/data/service/localstorage-browser.service";
+import { Observable } from "rxjs";
+import { Todo } from "../../features/todo/presentation/state/todos.state";
 
 export class ConsoleApp {
+  todos$: Observable<Array<Todo>>;
   todoPresenter: TodoPresenter;
   userPresenter: UserPresenter;
 
   constructor() {
     const inMemoryTodoRepo: TodoRepository = new TodoInMemoryRepository();
-    const localStorageTodoRepo: TodoRepository = new TodoLocalStorageRepository(
-      new LocalStorageBrowserService(window.localStorage)
-    );
-
-    const getAllTodosUC: GetAllTodosUseCase = new GetAllTodosUseCase(localStorageTodoRepo);
-    const addTodoUC: AddTodoUseCase = new AddTodoUseCase(localStorageTodoRepo);
-
-    const getCompletedTodosUC: GetCompletedTodosUseCase = new GetCompletedTodosUseCase(inMemoryTodoRepo);
-    const getIncompletedTodosUC: GetIncompletedTodosUseCase = new GetIncompletedTodosUseCase(inMemoryTodoRepo);
-    const searchTodosUC: SearchTodosUseCase = new SearchTodosUseCase(inMemoryTodoRepo);
-    const getTodoByIdUC: GetTodoByIdUseCase = new GetTodoByIdUseCase(inMemoryTodoRepo);
-    const removeTodoUC: RemoveTodoUseCase = new RemoveTodoUseCase(inMemoryTodoRepo);
-    const markTodoAsCompletedUC: MarkTodoAsCompletedUseCase = new MarkTodoAsCompletedUseCase(inMemoryTodoRepo);
-    const markTodoAsIncompletedUC: MarkTodoAsIncompletedUseCase = new MarkTodoAsIncompletedUseCase(inMemoryTodoRepo);
+    const getAllTodosUC: GetAllTodosUseCase = new GetAllTodosUseCase(inMemoryTodoRepo);
 
     this.todoPresenter = new TodoPresenter(
       getAllTodosUC,
-      getCompletedTodosUC,
-      getIncompletedTodosUC,
-      searchTodosUC,
-      addTodoUC,
-      getTodoByIdUC,
-      removeTodoUC,
-      markTodoAsCompletedUC,
-      markTodoAsIncompletedUC
     );
-
-    const inMemoryUserRepo: UserRepository = new UserInMemoryRepository();
-    const getAllUsersUC: GetAllUsersUseCase = new GetAllUsersUseCase(inMemoryUserRepo);
-    this.userPresenter = new UserPresenter(getAllUsersUC);
-
-    this.todoPresenter.todos$.subscribe(todos => {
-      console.log('todos:', todos);
-    });
-
-    this.todoPresenter.todo$.subscribe(todos => {
-      console.log('todo:', todos);
-    });
-
-    this.todoPresenter.errorMessage$.subscribe(error => {
-      console.log('got error:', error);
-    });
-
-    this.userPresenter.users$.subscribe(users => {
-      console.log('users:', users);
-    })
   }
 
   run() {
-    this.todoPresenter.addTodo('added 1');
-    this.todoPresenter.addTodo('added 2');
-    this.todoPresenter.removeTodo('1');
-    // this.todoPresenter.removeTodo('3');
-    this.todoPresenter.markTodoAsCompleted('2')
-    this.todoPresenter.markTodoAsCompleted('3')
-    // this.todoPresenter.searchTodos('2');
-    this.todoPresenter.getTodo('3');
-
-    this.todoPresenter.getCompletedTodos();
-    // this.todoPresenter.getIncompletedTodos();
-
-    this.userPresenter.getAllUsers();
-
-    this.todoPresenter.onDestroy();
-    this.userPresenter.onDestroy();
+    this.todoPresenter.getAllTodos();
   }
 }
--- src/features/todo/presentation/todo.presenter.ts
+++ src/features/todo/presentation/todo.presenter.ts
@@ -1,135 +1,29 @@
 import { GetAllTodosUseCase } from "../domain/usecase/get-all-todos.usecase";
-import { Subject, of, empty } from "rxjs";
+import { Subject, BehaviorSubject, Observable } from "rxjs";
 import { takeUntil, map, catchError } from "rxjs/operators";
-import { SearchTodosUseCase } from "../domain/usecase/search-todos.usecase";
-import { AddTodoUseCase } from "../domain/usecase/add-todo.usecase";
 import { TodoViewModelMapper } from "./todo.mapper";
 import { TodoViewModel } from "./todo.viewmodel";
-import { GetTodoByIdUseCase } from "../domain/usecase/get-todo-by-id.usecase";
-import { RemoveTodoUseCase } from "../domain/usecase/remove-todo-id.usecase";
-import { MarkTodoAsCompletedUseCase } from "../domain/usecase/mark-todo-as-complete.usecase";
-import { MarkTodoAsIncompletedUseCase } from "../domain/usecase/mark-todo-as-incomplete.usecase";
-import { GetCompletedTodosUseCase } from "../domain/usecase/get-completed-todos.usecase";
-import { GetIncompletedTodosUseCase } from "../domain/usecase/get-incompleted-todos.usecase";
 import { Presenter } from "../../../core/presentation/presenter";
+import { TodoState, Todo } from "./state/todos.state";
 
 export class TodoPresenter extends Presenter {
+  private state = new TodoState();
+  private dispatch = new BehaviorSubject<TodoState>(this.state);
+  
   todos$: Subject<Array<TodoViewModel>> = new Subject<Array<TodoViewModel>>();
-  todo$: Subject<TodoViewModel> = new Subject<TodoViewModel>();
-  errorMessage$: Subject<string> = new Subject<string>();
-  destroy$: Subject<any> = new Subject<any>();
   mapper = new TodoViewModelMapper();
 
   constructor(
     private getAllTodosUC: GetAllTodosUseCase,
-    private getCompletedTodosUC: GetCompletedTodosUseCase,
-    private getIncompletedTodosUC: GetIncompletedTodosUseCase,
-    private searchTodosUC: SearchTodosUseCase,
-    private addTodoUC: AddTodoUseCase,
-    private getTodoByIdUC: GetTodoByIdUseCase,
-    private removeTodoUC: RemoveTodoUseCase,
-    private markTodoAsCompletedUC: MarkTodoAsCompletedUseCase,
-    private markTodoAsIncompletedUC: MarkTodoAsIncompletedUseCase
   ) {
     super();
   }
 
-  getAllTodos() {
-    this.getAllTodosUC
+  getAllTodos(): Observable<Array<Todo>> {
+    return this.getAllTodosUC
       .execute()
       .pipe(
         map(todos => todos.map(this.mapper.mapFrom)),
-        takeUntil(this.destroy$)
       )
-      .subscribe(this.todos$);
-  }
-
-  getCompletedTodos() {
-    this.getCompletedTodosUC
-      .execute()
-      .pipe(
-        map(todos => todos.map(this.mapper.mapFrom)),
-        takeUntil(this.destroy$)
-      )
-      .subscribe(this.todos$);
-  }
-
-  getIncompletedTodos() {
-    this.getIncompletedTodosUC
-      .execute()
-      .pipe(
-        map(todos => todos.map(this.mapper.mapFrom)),
-        takeUntil(this.destroy$)
-      )
-      .subscribe(this.todos$);
-  }
-
-  searchTodos(keyword: string) {
-    this.searchTodosUC
-      .execute(keyword)
-      .pipe(
-        map(todos => todos.map(this.mapper.mapFrom)),
-        takeUntil(this.destroy$)
-      )
-      .subscribe(this.todos$);
-  }
-
-  addTodo(name: string) {
-    this.addTodoUC
-      .execute(name)
-      .pipe(
-        map(this.mapper.mapFrom),
-        takeUntil(this.destroy$)
-      )
-      .subscribe();
-  }
-
-  getTodo(id: string) {
-    this.getTodoByIdUC
-      .execute(id)
-      .pipe(
-        map(this.mapper.mapFrom),
-        takeUntil(this.destroy$),
-        catchError(err => {
-          this.errorMessage$.next(`Error occured: ${err}`);
-          return empty()
-        }),
-      )
-      .subscribe(this.todo$);
-  }
-
-  removeTodo(id: string) {
-    this.removeTodoUC
-      .execute(id)
-      .pipe(
-        map(this.mapper.mapFrom),
-        takeUntil(this.destroy$)
-      )
-      .subscribe();
-  }
-
-  markTodoAsCompleted(id: string) {
-    this.markTodoAsCompletedUC
-      .execute(id)
-      .pipe(
-        map(this.mapper.mapFrom),
-        takeUntil(this.destroy$)
-      )
-      .subscribe();
-  }
-
-  markTodoAsInCompleted(id: string) {
-    this.markTodoAsIncompletedUC
-      .execute(id)
-      .pipe(
-        map(this.mapper.mapFrom),
-        takeUntil(this.destroy$)
-      )
-      .subscribe();
-  }
-
-  onDestroy() {
-    this.destroy$.next();
-    this.destroy$.unsubscribe();
   }
 }
